8971d25d7049b0746cae3ec6c21bdbff1520ee29
Rohit Banda <rohit-banda@uiowa.edu>
2016-10-31 21:48:32 +0000

    test

--- app/controllers/board_controller_controller.rb
+++ app/controllers/board_controller_controller.rb
@@ -1,2 +1,11 @@
 class BoardControllerController < ApplicationController
+    
+    def board_params
+        params.require(:board).permit(:l1, :l2, :l3, :l4, :l5, :l6, :l7, :l8, :l9, :l10, :l11, :l12, :l13, :l14, :l15, :l16)
+    end
+    
+    def create
+        flash[:notice] = "Successfully created!"
+    end
+    
 end
